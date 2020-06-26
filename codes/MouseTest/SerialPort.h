8906eb82b581253a8134ad3ed11109ebd631048d
AI x Games <aixg_dev@outlook.com>
2020-06-26 18:18:48 +0800

    MouseTest debug

--- codes/MouseTest/SerialPort.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-HANDLE SerialPortOpen(const char* port, int baudrate);
-int SerialPortSend(HANDLE hcom, const void* buf, int len);
-int SerialPortReceive(HANDLE hcom, void* buf, int len);
